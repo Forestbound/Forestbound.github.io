1439e7de2cf465789cf8a80821209ca387c3aaa3
Forestbound <55318449+Forestbound@users.noreply.github.com>
2020-02-09 23:34:47 +0200

    Add files via upload

---
+++ waxom/src/js/app.js
@@ -0,0 +1,15 @@
+// Owl Carousel Script start
+$(".owl-carousel").owlCarousel({
+  autoplay: true,
+  autoplayTimeout: 8000,
+  autoplayHoverPause: true,
+  smartSpeed: 800,
+  loop: true,
+  nav: true,
+  responsive:{
+    0:{
+      items:1
+    }
+  }
+})
+// Owl Carousel Script end
